ab76c8f9c180c929a803f017feeb30964c6ee310
Parth Gajjar <parth@selcom.net>
2023-02-01 15:22:25 +0530

    Initial commit

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.svg') }}">
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
