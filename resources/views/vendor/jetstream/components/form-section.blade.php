f45ffa034440e6937b3786ffdefba832286615d0
Parth Gajjar <parth@selcom.net>
2023-03-02 13:16:58 +0530

    [change]: Responsive jetsream pages

--- resources/views/vendor/jetstream/components/form-section.blade.php
+++ resources/views/vendor/jetstream/components/form-section.blade.php
@@ -1,6 +1,6 @@
 @props(['submit'])
 
-<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6 divide-x-2']) }}>
+<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6 md:divide-x-2']) }}>
     <x-jet-section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
@@ -8,15 +8,15 @@
 
     <div class="mt-5 md:mt-0 md:col-span-2">
         <form wire:submit.prevent="{{ $submit }}">
-            <div class="px-4 py-5 sm:p-6 {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
-                <div class="grid grid-cols-6 gap-6">
+            <div class="px-4 py-5 sm:p-0  md:px-6 {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
+                <div class="grid grid-cols-6 gap-6 sm:grid-cols-1">
                     {{ $form }}
                 </div>
             </div>
 
             @if (isset($actions))
                 <div
-                    class="flex items-center justify-start px-4 py-3 text-right sm:px-6 sm:rounded-bl-md sm:rounded-br-md">
+                    class="flex items-center justify-start px-4 py-3 text-right sm:px-0 md:px-6 sm:rounded-bl-md sm:rounded-br-md">
                     {{ $actions }}
                 </div>
             @endif
