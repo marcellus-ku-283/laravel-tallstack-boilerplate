ef38e08abd0c5d9db39eb02a3db8a7e4987974a1
Parth Gajjar <gajjar.parth1811@gmail.com>
2023-03-29 17:02:41 +0530

    fix responsive css

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,12 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
+<div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+    <div class="mx-2">
+        <div>
+            {{ $logo }}
+        </div>
+
+        <div class="w-full p-6 overflow-hidden bg-white rounded-lg shadow-md">
+            {{ $slot }}
+        </div>
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
-    </div>
 </div>
